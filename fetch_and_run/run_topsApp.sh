2f9473c6a2697f6c47d1148686ceaf4660648991
Scott Henderson <scottyhq@gmail.com>
2020-04-14 22:35:18 +0000

    add fetch_and_run folder

---
+++ fetch_and_run/run_topsApp.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#S3_INPUT=s3://dinosar/processing/test/int-20180706-20180624/
+#S3_OUTPUT=s3://dinosar/results/uniongap/int-20180706-20180624/
+#S3_DEM=s3://dinosar/processing/uniongap/dem
+
+# Mounted EBS volume
+echo "Processing disk space available:"
+df -sh
+cd /opt/scratch
+
+echo "Syncing files from ${S3_INTPUT}"
+aws s3 sync ${S3_INPUT} .
+
+echo "Syncing dem from ${S3_DEM}"
+aws s3 sync ${S3_DEM} .
+
+echo "Downloading SLCs and Orbits..."
+aria2c -x 8 -s 8 -i download-links.txt
+
+echo "Running topsApp..."
+#topsApp.py 2>&1 | tee topsApp.log
+# Mock for testing
+mkdir merged/
+echo "Results!" > merged/results.txt
+
+echo "Syncing results to ${S3_OUTPUT} ..."
+cp topsApp.xml topsApp.log topsProc.xml download-links.txt merged/
+aws s3 sync merged ${S3_OUTPUT}
+
+echo "Done!"
